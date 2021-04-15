6ca9e1bf20ac038a742febc619301623f7688cc5
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2021-04-16 03:33:27 +0700

    add model brand and test jsoup

--- src/main/java/com/khangse616/crawldatazanado/CrawldatazanadoApplication.java
+++ src/main/java/com/khangse616/crawldatazanado/CrawldatazanadoApplication.java
@@ -1,13 +1,52 @@
 package com.khangse616.crawldatazanado;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class CrawldatazanadoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrawldatazanadoApplication.class, args);
-    }
+        try {
+            Document doc = Jsoup.connect("https://www.zanado.com/bo-do-the-thao-nang-dong-colo-cl1703339-dep-gia-tot-giam-30-sid65674.html?color=77").get();
 
+//            Elements topbar = doc.select("div.top-bar > div.breadcrumbs > ul > li");
+//
+//            Map<Integer, String> mapCategories = new HashMap<>();
+//
+//            for(int i = 1; i<topbar.size();i++){
+//                Element li = topbar.get(i);
+//
+//                int idCategory = Integer.parseInt(li.attr("class").split(" ")[0].replace("category", ""));
+//                String nameCategory = li.select("span").text();
+//
+//                mapCategories.put(idCategory, nameCategory);
+//            }
+
+
+            Elements main = doc.select("div.main div#productleft");
+
+            Elements productPriceView = main.select("div.blockhead div.detail-text div.product-priceview");
+
+            String[] nameId = productPriceView.select("div.product-name h1").text().split(" SID");
+            String name = nameId[0];
+            int id = Integer.parseInt(nameId[1]);
+
+            System.out.println("id: "+id+" - name: "+name);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/khangse616/crawldatazanado/models/Brand.java
@@ -0,0 +1,44 @@
+package com.khangse616.crawldatazanado.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "brands")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Brand {
+    @Id
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Brand() {
+    }
+
+    public Brand(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
