014df5734e285899df18a9527bb1954db7a79cb5
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2021-04-15 16:34:47 +0700

    create new project

---
+++ src/main/java/com/khangse616/crawldatazanado/CrawldatazanadoApplication.java
@@ -0,0 +1,13 @@
+package com.khangse616.crawldatazanado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawldatazanadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawldatazanadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khangse616/crawldatazanado/CrawldatazanadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khangse616.crawldatazanado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawldatazanadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
